868a90225a157aebd3ed25888be147a2eb35a0e5
HuberM1998 <huber.mathaus@gmail.com>
2022-01-26 22:45:10 -0300

    edição de informações de usuario

---
+++ src/views/EditarUser.vue
@@ -0,0 +1,24 @@
+<template>
+  <form-user :user="user" />
+</template>
+
+<script>
+import FormUser from "../components/forms/FormUser.vue";
+import { getUser } from "../services/api";
+
+export default {
+  components: { FormUser },
+  data() {
+    return { user: {} };
+  },
+  created() {
+    getUser()
+      .then(({ data }) => (this.user = data))
+      .catch(() => {
+        this.$router.push("/");
+      });
+  },
+};
+</script>
+
+<style></style>
