264373f6e1628c134fb3b33fbf492c7f5ebc3674
HuberM1998 <huber.mathaus@gmail.com>
2022-01-26 22:44:58 -0300

    edição de informações do pet

---
+++ src/views/EditarPet.vue
@@ -0,0 +1,29 @@
+<template>
+  <form-pet :pet="pet" />
+</template>
+
+<script>
+import FormPet from "../components/forms/FormPet.vue";
+import { getPet } from "../services/api";
+export default {
+  name: "EditarPet",
+  components: { FormPet },
+  props: ["petId"],
+  data() {
+    return {
+      pet: {},
+    };
+  },
+  created() {
+    getPet(this.petId)
+      .then(({ data }) => {
+        this.pet = data;
+      })
+      .catch(() => {
+        this.$router.push("/dashboard");
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
