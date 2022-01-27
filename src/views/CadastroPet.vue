ebdf3ba3947242e4c34980b32d5617060cb4c5b7
HuberM1998 <huber.mathaus@gmail.com>
2022-01-26 22:44:24 -0300

    página de cadastro de pets

---
+++ src/views/CadastroPet.vue
@@ -0,0 +1,13 @@
+<template>
+  <form-pet />
+</template>
+
+<script>
+import FormPet from "../components/forms/FormPet";
+export default {
+  components: {
+    FormPet,
+  },
+};
+</script>
+<style lang="scss" scoped></style>
